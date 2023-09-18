e8384e91a6e1034f5622f0e844b3299ae9f485b6
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-09-18 18:40:33 +0300

    working with API D O N E

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -8,8 +8,19 @@ class GetPrograms:
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
     response = requests.get(URL)
+    # return json.dumps(json.loads(response.content),indent=4)
     return response.content
+  def program_school(self):
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+      programs_list.append(program['agency'])
+    return programs_list[3]
 
 
-programs = GetPrograms().get_programs()
-print(programs)
+
+
+all_programs = GetPrograms().get_programs()
+programs = GetPrograms().program_school()
+for program in set(programs):
+  print(program)
